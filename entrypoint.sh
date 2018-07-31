20f879e1b35f1f5fe6096fe01125e1cffb50223d
dan make <danmakes2022@gmail.com>
2018-07-31 10:44:34 +0200

    Adding stage variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 set -e
 sed -i -e "s~__APP_SERVER_URI__~${APP_SERVER_URI}~g" /usr/share/nginx/html/index.html
+sed -i -e "s~__APP_STAGE__~${APP_STAGE}~g" /usr/share/nginx/html/index.html
 
 exec "$@"
